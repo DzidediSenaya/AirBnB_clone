8aaf7dde3e3a141cc1a2327f0baf5d98f5103ae0
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-14 02:44:33 +0000

    Update console.py

--- console.py
+++ console.py
@@ -99,7 +99,7 @@ class HBNBCommand(cmd.Cmd):
         del obj_dict[obj_key]
         storage.save()
 
-   def do_all(self, arg):
+    def do_all(self, arg):
     """Usage: all or all <class>
     Display string representations of all instances of a given class.
     If no class is specified, displays all instantiated objects."""
