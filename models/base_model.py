e5edaf96ef00d543786b9bc4a66a9bdd0e52cee0
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-14 00:23:02 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,70 +2,54 @@
 """
 This module defines the BaseModel class.
 """
+
+
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    """
-    BaseModel class definition.
-    """
+    """A base class that defines common attributes and methods for other classes."""
+
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new BaseModel instance.
-
-        Args:
-            *args: Unused positional arguments.
-            **kwargs: Keyword arguments containing attributes to set.
-
-        Attributes:
-            id (str): The ID of the instance.
-            created_at (datetime): The creation datetime of the instance.
-            updated_at (datetime): The last update datetime of the instance.
-        """
+        """Initialize instance attributes."""
         if kwargs:
-            kwargs['created_at'] = datetime.strptime(
-                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f'
-            )
-            kwargs['updated_at'] = datetime.strptime(
-                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f'
-            )
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
                     setattr(self, key, value)
-            storage.new(self)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
-
-    def to_dict(self):
-        """
-        Returns a dictionary representation of the instance.
-
-        Returns:
-            dict: A dictionary containing the instance attributes.
-        """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.strftime(
-            '%Y-%m-%dT%H:%M:%S.%f'
-        )
-        obj_dict['updated_at'] = self.updated_at.strftime(
-            '%Y-%m-%dT%H:%M:%S.%f'
-        )
-        return obj_dict
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Returns a string representation of the instance.
+        """Return a string representation of the instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
-        Returns:
-            str: A formatted string containing class name,
-            ID, and attributes.
-        """
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__,
-            self.id,
-            self.__dict__
-            )
+    def save(self):
+        """Update the public instance attribute 'updated_at' with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of the instance."""
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
+
+# Test code
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
