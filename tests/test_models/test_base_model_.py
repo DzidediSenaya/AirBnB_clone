0eae81b6560d2c947fce8505d2eaaa25dee85cf8
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-14 00:38:16 +0000

    Update test_base_model_.py

--- tests/test_models/test_base_model_.py
+++ tests/test_models/test_base_model_.py
@@ -2,15 +2,45 @@
 """
 This module contains test cases for the BaseModel class and its methods.
 """
-import unittest
-from models.base_model import BaseModel
+import uuid
+from datetime import datetime
 from models import storage
 
 
-class TestBaseModel(unittest.TestCase):
-    """
-    Test cases for the BaseModel class.
-    """
+class BaseModel:
+    """A base class that defines common attributes and methods for other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize instance attributes."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update the public instance attribute 'updated_at' with the current datetime."""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of the instance."""
+        instance_dict = self.__dict__.copy()
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
+        return instance_dict
 
     def test_base_model_attributes(self):
         """
