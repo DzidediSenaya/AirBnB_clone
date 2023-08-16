76e4cf8733e5913015b9e12989b35bb68c8eb889
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-16 01:56:28 +0000

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,7 +3,6 @@
 
 import unittest
 
-
 class ConsoleTestCase(unittest.TestCase):
     def test_quit(self):
         self.assertTrue(some_condition, "Quit command not implemented")
@@ -18,47 +17,46 @@ class ConsoleTestCase(unittest.TestCase):
         self.assertTrue(some_condition, "Empty line handling not implemented")
 
     def test_base_model_count(self):
-        self.assertTrue(some_condition, "Create base model command not implemented")
+        self.assertTrue(some_condition, "Base model count command not implemented")
 
     def test_user_count(self):
-       #fill later
+        self.assertTrue(some_condition, "User count command not implemented")
 
-    def 1State.count(self):
-        #fill later
+    def test_state_count(self):
+        self.assertTrue(some_condition, "State count command not implemented")
 
-    def Place.count(self):
-        #fill later
+    def test_place_count(self):
+        self.assertTrue(some_condition, "Place count command not implemented")
 
-    def City.count(self):
-      #fill later
+    def test_city_count(self):
+        self.assertTrue(some_condition, "City count command not implemented")
 
-    def Amenity.count(self):
-     #fill later
+    def test_amenity_count(self):
+        self.assertTrue(some_condition, "Amenity count command not implemented")
 
-    def User.show("id"):
-        #tests go here
+    def test_user_show(self):
+        self.assertTrue(some_condition, "User show command not implemented")
 
-    def State.show("id"):
-        #tests go here
+    def test_state_show(self):
+        self.assertTrue(some_condition, "State show command not implemented")
 
-    def City.show("id"):
-       #tests go here, fill later
+    def test_city_show(self):
+        self.assertTrue(some_condition, "City show command not implemented")
 
-    def Amenity.show("id"):
-       #fill later
+    def test_amenity_show(self):
+        self.assertTrue(some_condition, "Amenity show command not implemented")
 
-    def Place.show("id"):
-        #fill later
+    def test_place_show(self):
+        self.assertTrue(some_condition, "Place show command not implemented")
 
-    def Place.destroy("id"):
-        #fill later
+    def test_place_destroy(self):
+        self.assertTrue(some_condition, "Place destroy command not implemented")
 
-    def Amenity.destroy("id")(self):
-       #fill later
+    def test_amenity_destroy(self):
+        self.assertTrue(some_condition, "Amenity destroy command not implemented")
 
-    def Review.destroy("id")(self):
-       #fill later
-
+    def test_review_destroy(self):
+        self.assertTrue(some_condition, "Review destroy command not implemented")
 
 if __name__ == '__main__':
     unittest.main()
