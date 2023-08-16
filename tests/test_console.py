c52ef64cd5bc1277fb8d2232976fab391871d7e7
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-16 01:52:58 +0000

    mock test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -17,47 +17,47 @@ class ConsoleTestCase(unittest.TestCase):
     def test_empty_line(self):
         self.assertTrue(some_condition, "Empty line handling not implemented")
 
-    def test_create_base_model(self):
+    def test_base_model_count(self):
         self.assertTrue(some_condition, "Create base model command not implemented")
 
-    def test_show_base_model(self):
-        self.assertTrue(some_condition, "Show base model command not implemented")
-
-    def test_destroy_base_model(self):
-        self.assertTrue(some_condition, "Destroy base model command not implemented")
-
-    def test_all_base_model(self):
-        self.assertTrue(some_condition, "All base models command not implemented")
-
-    def test_update_base_model(self):
-        self.assertTrue(some_condition, "Update base model command not implemented")
-
-    def test_base_model_all(self):
-        self.assertTrue(some_condition, "All base models command not implemented")
-
-    def test_review_all(self):
-        self.assertTrue(some_condition, "All reviews command not implemented")
-
-    def test_user_all(self):
-        self.assertTrue(some_condition, "All users command not implemented")
-
-    def test_state_all_present(self):
-        self.assertTrue(some_condition, "All states command not implemented")
-
-    def test_city_all(self):
-        self.assertTrue(some_condition, "All cities command not implemented")
-
-    def test_amenity_all(self):
-        self.assertTrue(some_condition, "All amenities command not implemented")
-
-    def test_place_all(self):
-        self.assertTrue(some_condition, "All places command not implemented")
-
-    def test_base_model_count(self):
-        self.assertTrue(some_condition, "Base model count command not implemented")
-
     def test_user_count(self):
-        self.assertTrue(some_condition, "User count command not implemented")
+       #fill later
+
+    def 1State.count(self):
+        #fill later
+
+    def Place.count(self):
+        #fill later
+
+    def City.count(self):
+      #fill later
+
+    def Amenity.count(self):
+     #fill later
+
+    def User.show("id"):
+        #tests go here
+
+    def State.show("id"):
+        #tests go here
+
+    def City.show("id"):
+       #tests go here, fill later
+
+    def Amenity.show("id"):
+       #fill later
+
+    def Place.show("id"):
+        #fill later
+
+    def Place.destroy("id"):
+        #fill later
+
+    def Amenity.destroy("id")(self):
+       #fill later
+
+    def Review.destroy("id")(self):
+       #fill later
 
 
 if __name__ == '__main__':
